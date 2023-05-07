700ac0b8549850ccd449d0fb209303d3acdb76d7
LiuXilin-Frankie <118020043@link.cuhk.edu.cn>
2023-05-07 18:31:14 +0800

    check orderbook.py

--- ready_trader_go/order_book.py
+++ ready_trader_go/order_book.py
@@ -87,8 +87,8 @@ class OrderBook(object):
         self.__bid_prices: List[int] = []
         self.__bid_ticks: Dict[int, int] = collections.defaultdict(int)
         self.__last_traded_price: Optional[int] = None
-        self.__levels: Dict[int, Deque[Order]] = {}
-        self.__total_volumes: Dict[int, int] = {}
+        self.__levels: Dict[int, Deque[Order]] = {}         # 字典，对应价格以及order队列
+        self.__total_volumes: Dict[int, int] = {}           # 字典，记录价格对应的所有订单量
 
         # Signals
         self.trade_occurred: List[Callable[[Any], None]] = list()
@@ -105,7 +105,11 @@ class OrderBook(object):
                 + "\n" + "\n".join("%6d\t%dc" % (v, p) for p, v in zip(bid_prices, bid_volumes) if p))
 
     def amend(self, now: float, order: Order, new_volume: int) -> None:
-        """Amend an order in this order book by decreasing its volume."""
+        """
+        Amend an order in this order book by decreasing its volume.
+        通过减少现有订单量来修改订单（只能减少订单（因为增加订单量涉及排队先后问题））
+        如果已经成交的成交量要高于希望修改的新订单量，那我们实际上只能取消该订单
+        """
         if order.remaining_volume > 0:
             fill_volume = order.volume - order.remaining_volume
             diff = order.volume - (fill_volume if new_volume < fill_volume else new_volume)
@@ -124,7 +128,10 @@ class OrderBook(object):
         return self.__bid_prices[-1] if self.__bid_prices else None
 
     def cancel(self, now: float, order: Order) -> None:
-        """Cancel an order in this order book."""
+        """
+        Cancel an order in this order book.
+        取消订单，直接将未成交的全部删除
+        """
         if order.remaining_volume > 0:
             self.remove_volume_from_level(order.price, order.remaining_volume, order.side)
             remaining = order.remaining_volume
@@ -133,9 +140,15 @@ class OrderBook(object):
                 order.listener.on_order_cancelled(now, order, remaining)
 
     def insert(self, now: float, order: Order) -> None:
-        """Insert a new order into this order book."""
+        """
+        Insert a new order into this order book.
+        插入新的订单
+        
+        如果下单的价格可以直接进行成交，则先进行交易
+        如果交易后仍然存在订单量，则将剩余的订单挂单到订单薄上
+        """
         if order.side == Side.SELL and self.__bid_prices and order.price <= self.__bid_prices[-1]:
-            self.trade_ask(now, order)
+            self.trade_ask(now, order)      # if self.__bid_prices 保证空集情况下不会被处理
         elif order.side == Side.BUY and self.__ask_prices and order.price >= self.__ask_prices[-1]:
             self.trade_bid(now, order)
 
@@ -159,7 +172,11 @@ class OrderBook(object):
         return None
 
     def place(self, now: float, order: Order) -> None:
-        """Place an order that does not match any existing order in this order book."""
+        """
+        Place an order that does not match any existing order in this order book.
+        如果订单不能或不能完全与订单薄上现有订单成交 我们则将其place进订单薄中
+        同时需要更改orderbook中其它的属性参数
+        """
         price = order.price
 
         if price not in self.__levels:
@@ -177,6 +194,13 @@ class OrderBook(object):
             order.listener.on_order_placed(now, order)
 
     def remove_volume_from_level(self, price: int, volume: int, side: Side) -> None:
+        """
+        删除某价格的固定数量的订单
+
+        如果要删除的与 orderbook 上剩余数量正好相等，则删除全部对应信息
+        如果不相等，则只删除对应数量
+        对应的level以及total_volumes统计信息也会删除这一个price段
+        """
         if self.__total_volumes[price] == volume:
             del self.__levels[price]
             del self.__total_volumes[price]
@@ -189,7 +213,11 @@ class OrderBook(object):
 
     def top_levels(self, ask_prices: List[int], ask_volumes: List[int], bid_prices: List[int],
                    bid_volumes: List[int]) -> None:
-        """Populate the supplied lists with the top levels for this book."""
+        """
+        Populate the supplied lists with the top levels for this book.
+        获取最优 TOP_LEVEL_COUNT 数量的订单报价以及数量
+        如果订单薄是稀疏的 只会获取有单的 TOP_LEVEL_COUNT 档订单
+        """
         i = 0
         j = len(self.__ask_prices) - 1
         while i < TOP_LEVEL_COUNT and j >= 0:
@@ -213,7 +241,10 @@ class OrderBook(object):
             i += 1
 
     def trade_ask(self, now: float, order: Order) -> None:
-        """Check to see if any existing bid orders match the specified ask order."""
+        """
+        Check to see if any existing bid orders match the specified ask order.
+        检查是否存在匹配的bid单可以跟新到达的ask单进行交易
+        """
         best_bid = self.__bid_prices[-1]
 
         while order.remaining_volume > 0 and best_bid >= order.price and self.__total_volumes[best_bid] > 0:
@@ -222,9 +253,9 @@ class OrderBook(object):
                 del self.__levels[best_bid]
                 del self.__total_volumes[best_bid]
                 self.__bid_prices.pop()
-                if not self.__bid_prices:
+                if not self.__bid_prices:   #如果删除后列表为空，则break循环
                     break
-                best_bid = self.__bid_prices[-1]
+                best_bid = self.__bid_prices[-1]    #如果列表不为空，则更新best_bid
 
     def trade_bid(self, now: float, order: Order) -> None:
         """Check to see if any existing ask orders match the specified bid order."""
@@ -241,15 +272,18 @@ class OrderBook(object):
                 best_ask = -self.__ask_prices[-1]
 
     def trade_level(self, now: float, order: Order, best_price: int) -> None:
-        """Match the specified order with existing orders at the given level."""
+        """
+        Match the specified order with existing orders at the given level.
+        在特定的level下成交订单
+        """
         remaining: int = order.remaining_volume
         order_queue: Deque[Order] = self.__levels[best_price]
         total_volume: int = self.__total_volumes[best_price]
 
         while remaining > 0 and total_volume > 0:
             while order_queue[0].remaining_volume == 0:
-                order_queue.popleft()
-            passive: Order = order_queue[0]
+                order_queue.popleft() #把订单列表中已经成交完的订单删除
+            passive: Order = order_queue[0]     # 更改 passive 中的属性会一起更改原来的Order实例，代表这是一次浅复制
             volume: int = remaining if remaining < passive.remaining_volume else passive.remaining_volume
             fee: int = round(best_price * volume * self.maker_fee)
             total_volume -= volume
